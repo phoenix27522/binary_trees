7b6634fb37912d55bc0835048e5625ab75f57b13
Nahom kiflu <kiflunahom1994@gmail.com>
2024-01-03 11:14:01 +0300

    create tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,24 @@
-#include bina
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates binary tree
+ * @parent: root of the binarry tree
+ * @value: value of the nodes
+ *
+ * Return: binary tree
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->n = value;
+	newnode->left = NULL;
+	newnode->right = NULL;
+	newnode->parent = parent;
+	return (newnode);
+
+}
